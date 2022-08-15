40981343f02a417dc421f42e6902a14810de5c48
Teodor Heggelund <git@teod.eu>
2022-08-15 16:36:58 +0200

    watch.sh - show help when arguments are missing

--- watch.sh
+++ watch.sh
@@ -3,6 +3,20 @@
 sleep_time=$1
 shift
 
+if [ -z $sleep_time ]; then
+    echo "Usage:"
+    echo ""
+    echo "  ./watch.sh SLEEP_TIME COMMAND..."
+    exit 1
+fi
+
+if [ -z $1 ]; then
+    echo "Usage:"
+    echo ""
+    echo "  ./watch.sh SLEEP_TIME COMMAND..."
+    exit 1
+fi
+
 while true; do
     clear
     $@
