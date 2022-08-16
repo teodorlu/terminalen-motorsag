c4450b4a6ed4156b8739ef3fca38fbcddb346394
Magnus Reier Rushfeldt <magnus.reier.rushfeldt@iterate.no>
2022-08-16 14:55:53 +0200

    Add empty message.sh script, with description of usage

---
+++ message.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# USAGE: 
+#   ./message [MESSAGE]
+# 
+#   Creates a file with the name "msg-XX.txt" (where XX is a serial number)
+#   in the directory "messages", with the following content:
+#
+#   MESSAGE\n
+#   
+#   Example:
+#       input:
+#           ./message "hei, jeg heter teodor"
+#       output:
+#           a new file "messages/msg-01.txt", with the contents
+#
+#           1   hei, jeg heter teodor
+#           2   
+#
+
