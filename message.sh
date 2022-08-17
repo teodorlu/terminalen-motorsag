ababaf204efce7eade6ca0493a14c8b69875d0b0
Magnus Reier Rushfeldt <magnus.reier.rushfeldt@iterate.no>
2022-08-17 10:45:52 +0200

    Add help msg to message.sh when called w/o args

--- message.sh
+++ message.sh
@@ -21,10 +21,17 @@
 
 set -e
 
+if [ -z $1 ]; then
+    echo "Usage:"
+    echo ""
+    echo "./message.sh MESSAGE"
+    exit 1
+fi
+
 file_name=$(find messages -type f | grep msg- | grep "\d" | sort -r | grep "\d*" -m1 -o)
 
 file_name=$((file_name + 1))
 
-cd ~/terminalen-motorsag/messages
+cd ./messages
 touch msg-$file_name.txt
 echo $1 > msg-$file_name.txt
