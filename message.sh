2ebe4b0db081ef17db8bffdc0e35c9f1179938e3
Magnus Reier Rushfeldt <magnus.reier.rushfeldt@iterate.no>
2022-08-17 12:04:11 +0200

    Update message.sh to use epoch seconds for filenum

--- message.sh
+++ message.sh
@@ -16,26 +16,7 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-# STEPS USED IN CREATING FILE NUMBER:
-# 
-# find messages -type f
-#   - We need to get an overview of what files there are in messages folder.
-# |
-#   - The pipe operator sends the output of the preceding command to the input of the succeeding command.
-# grep "msg-\d"
-#   - Of all files, we only want to have the numbered message files (msg-NN.txt).
-# sort -r
-#   - To get the highest-numbered message file on top, we need to order the files as such.
-# grep "\d*" -m1 -o
-#   - We only want the single message with the highest number, and only the number part.
-#     This is the number we will use to give the correct serial number to the next file.
-# $(...)
-#   - The result of the chain of the commands above must be evaluated and the resulting number returned.
-# $(( $(...) + 1 ))
-#   - The previous file number is incremented by 1 to get the next file number.
-#     This arithmetic operation is evaluated and returned as a number. 
-
-file_number=$(($(find messages -type f | grep "msg-\d" | sort -r | grep "\d*" -m1 -o) + 1))
+file_number=$(date "+%s")
 
 cd ./messages
 touch msg-$file_number.txt
