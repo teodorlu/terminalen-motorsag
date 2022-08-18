8f382b6028a2e9c05e1d1c5e62effcb7232e2860
Teodor Heggelund <git@teod.eu>
2022-08-18 11:14:34 +0200

    Fix watch

--- watch.sh
+++ watch.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
+
 set -e # Terminate script when something goes wrong
 
-sleep_time=$1
+if [ -z "$1" ]; then
+    echo "Usage:"
+    echo ""
+    echo "  ./watch.sh SLEEP_TIME COMMAND..."
+    exit 1
+else
+    echo "no match"
+fi
+
+sleep_time="$1"
 shift
-
-if [ -z $sleep_time ]; then
-    echo "Usage:"
-    echo ""
-    echo "  ./watch.sh SLEEP_TIME COMMAND..."
-    exit 1
-fi
-
-if [ -z $1 ]; then
-    echo "Usage:"
-    echo ""
-    echo "  ./watch.sh SLEEP_TIME COMMAND..."
-    exit 1
-fi
 
 while true; do
     clear
-    $@
-    sleep $sleep_time
+    "$@"
+    sleep "$sleep_time"
 done
