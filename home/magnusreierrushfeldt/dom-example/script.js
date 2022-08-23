ea357de92a2e8985585dfcd64b69604702a2c507
Magnus Reier Rushfeldt <magnus.reier.rushfeldt@iterate.no>
2022-08-23 14:10:58 +0200

    Add listener for generating slides on "p" press

--- home/magnusreierrushfeldt/dom-example/script.js
+++ home/magnusreierrushfeldt/dom-example/script.js
@@ -1,24 +1,26 @@
-function create_slides(nodes){
-    const slides = []
-    for (var i = 0; i < nodes.length; i++){
-
-        if (!nodes[i]){
-            continue;
-        }
-
-        let lastIndex = slides.length - 1
-        if (nodes[i].tagName === "H1") {
-            let slide = {title: nodes[i].innerText, body: ""}
-            slides.push(slide) 
-        }
-        else if (nodes[i].tagName === "P" && slides[lastIndex]) {
-            slides[lastIndex].body += nodes[i].innerText + "\n";
-        }
-    }
-    return slides
-}
-
 let slideshow = document.getElementById("slideshow");
-let nodes = slideshow.children;
 
-const slides = create_slides(nodes);
+const create_slides = (nodes) => {
+  const slides = [];
+  for (var i = 0; i < nodes.length; i++) {
+    if (!nodes[i]) {
+      continue;
+    }
+
+    let lastIndex = slides.length - 1;
+    if (nodes[i].tagName === "H1") {
+      let slide = { title: nodes[i].innerText, body: "" };
+      slides.push(slide);
+    } else if (nodes[i].tagName === "P" && slides[lastIndex]) {
+      slides[lastIndex].body += nodes[i].innerText + "\n";
+    }
+  }
+  return slides;
+};
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "p") {
+    const slides = create_slides(slideshow.children);
+    console.log("Slides data structure generated from current page:\n", slides);
+  }
+});
