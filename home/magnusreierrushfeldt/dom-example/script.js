25192869d127cb435909cb99a859e6f4ea46891c
Magnus Reier Rushfeldt <magnus.reier.rushfeldt@iterate.no>
2022-08-23 13:57:38 +0200

    Create script for building slides data structure

--- home/magnusreierrushfeldt/dom-example/script.js
+++ home/magnusreierrushfeldt/dom-example/script.js
@@ -1,3 +1,24 @@
-let slideshow = document.getElementById("slideshow");
+function create_slides(nodes){
+    const slides = []
+    for (var i = 0; i < nodes.length; i++){
 
-console.log(slideshow)
+        if (!nodes[i]){
+            continue;
+        }
+
+        let lastIndex = slides.length - 1
+        if (nodes[i].tagName === "H1") {
+            let slide = {title: nodes[i].innerText, body: ""}
+            slides.push(slide) 
+        }
+        else if (nodes[i].tagName === "P" && slides[lastIndex]) {
+            slides[lastIndex].body += nodes[i].innerText + "\n";
+        }
+    }
+    return slides
+}
+
+let slideshow = document.getElementById("slideshow");
+let nodes = slideshow.children;
+
+const slides = create_slides(nodes);
