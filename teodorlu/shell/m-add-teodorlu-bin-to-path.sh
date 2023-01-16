291867932c341915c3d3713115bb87552221d435
Teodor Heggelund <git@teod.eu>
2023-01-16 13:26:45 +0100

    Move shell stuff to teodorlu/shell

--- teodorlu/shell/m-add-teodorlu-bin-to-path.sh
+++ teodorlu/shell/m-add-teodorlu-bin-to-path.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env sh
-
 PATH="$(git rev-parse --show-toplevel)/teodorlu/bin":$PATH
---
+++ teodorlu/shell/prompt-simple.sh
@@ -0,0 +1,6 @@
+# For å bruke denne:
+#
+#     source teodorlu/shell/prompt-simple.sh
+
+prompt off
+PS1='$ '
